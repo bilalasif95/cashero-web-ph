55778f9c0bf78005256aa2fda714e11afe57c775
Bilal Asif <bilal@rnssol.com>
2021-02-11 17:16:09 +0500

    sitemap.xml file added

--- packages/mars-theme/src/sitemap-generator.js
+++ packages/mars-theme/src/sitemap-generator.js
@@ -8,7 +8,7 @@ const Sitemap = require("react-router-sitemap").default;
 function generateSitemap() {
   return new Sitemap(router)
     .build("https://staging.cashero.com/")
-    .save("./build/sitemap.xml");
+    .save("./sitemap.xml");
 }
 
 generateSitemap();
