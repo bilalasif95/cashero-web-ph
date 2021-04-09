8240e26322e861c8d1cd5f0bd65b0a20e096ae7b
Bilal Asif <bilal@rnssol.com>
2021-04-09 10:27:49 +0500

    latest code pulled from master branch

--- packages/mars-theme/src/config/config.js
+++ packages/mars-theme/src/config/config.js
@@ -4,12 +4,12 @@ const API = process.env.REACT_APP_API_URL
 const firebaseAPI =
   "https://firestore.googleapis.com/v1/projects/cashero-828/databases/(default)/documents/";
 
-export const websiteLink = "https://www.cashero.com";
+export const websiteLink = "https://staging.cashero.com";
 
 export const wordPressLink = "https://wp.cashero.com";
 
-export const gtmId = "GTM-MBGVT62";
-//for staging.cashero.com   GTM-KKSRK7H
+export const gtmId = "GTM-KKSRK7H";
+//for cashero.com   GTM-MBGVT62
 
 export const EndPoints = {
   preregistration: `${API + "/api/users/preregistration"}`,
