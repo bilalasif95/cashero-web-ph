9454040278101f6f781b80ea4dbddc0f605febf9
Bilal Asif <bilal@rnssol.com>
2021-04-14 22:25:58 +0500

    ticket#135 resolved

--- packages/mars-theme/src/components/index.js
+++ packages/mars-theme/src/components/index.js
@@ -38,7 +38,7 @@ const Theme = ({ state }) => {
   const data = state.source.get(state.router.link);
   useEffect(() => {
     let name = 'cashero-web'
-    let version = '1.0.20'
+    let version = '1.0.21'
     const last_version = localStorage.getItem(`${name}-Version`)
     if (!last_version) {
       window.location.reload();
