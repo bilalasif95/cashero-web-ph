8ef31b4218c62377c6bdc89cc2e3aec2978e6f1c
WaqasHakeem <waqas@rnssol.com>
2021-05-19 11:43:44 +0500

    gif optimized

--- packages/mars-theme/src/components/HomePage/HomePage.js
+++ packages/mars-theme/src/components/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import TextfieldBanner from "../TextfieldBanner/TextfieldBanner";
-import Banner from "../../assets/photoshop.gif";
+import CasheroIntro from "../../assets/cashero-intro.gif";
 // import casheromain from "../../assets/casheromain.png";
 import Check from "../../assets/check-mark.svg";
 import GlobalMoney from "../GlobalMoney/GlobalMoney";
@@ -63,7 +63,7 @@ const HomePage = ({ i18n }) => {
             </div>
             <div className="col-md-4">
               <div className="bannerImg">
-                  <img className="img-fluid gif" alt="Banner" src={Banner} />
+                  <img className="img-fluid gif" alt="Banner" src={CasheroIntro} />
                   {/* <img className="img-fluid cash-pic" alt="Banner" src={casheromain} /> */}
               </div>
               </div>
