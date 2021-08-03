646e6b9f16a6a3679491901b475191d674d31ee8
Bilal Asif <bilal@rnssol.com>
2021-08-03 10:01:08 +0500

    personal and donations tab reverted + Ticket#812 resolved

--- packages/mars-theme/src/components/list/list.js
+++ packages/mars-theme/src/components/list/list.js
@@ -193,7 +193,7 @@ const List = ({ i18n, state }) => {
               id="navbarSupportedContent"
             >
               <ul className="navbar-nav ml-auto">
-                {/* <li className="nav-item dropdown disabled" onClick={innerWidth < 992 ? onPersonalToggleButtonClicked : null}>
+                <li className="nav-item dropdown" onClick={innerWidth < 992 ? onPersonalToggleButtonClicked : null}>
                   <a
                     className="nav-link dropdown-toggle"
                     id="PersonalDropdown"
@@ -203,8 +203,8 @@ const List = ({ i18n, state }) => {
                     aria-expanded="false"
                   >
                     {i18n.t("Personal")}
-                  </a> */}
-                  {/* <div
+                  </a>
+                  <div
                     className={personalToggleBotton ? "dropdown-menu toggleButtonShow" : "dropdown-menu"}
                     aria-labelledby="PersonalDropdown"
                   >
@@ -261,8 +261,8 @@ const List = ({ i18n, state }) => {
                         </div>
                       </div>
                     </div>
-                  </div> */}
-                {/* </li> */}
+                  </div>
+                </li>
                 <li className="nav-item dropdown" onClick={innerWidth < 992 ? onBusinessToggleButtonClicked : null}>
                   <a
                     className="nav-link dropdown-toggle"
@@ -333,7 +333,7 @@ const List = ({ i18n, state }) => {
                     </div>
                   </div>
                 </li>
-                {/* <li className="nav-item dropdown disabled" onClick={innerWidth < 992 ? onDonationsToggleButtonClicked : null}>
+                <li className="nav-item dropdown" onClick={innerWidth < 992 ? onDonationsToggleButtonClicked : null}>
                   <a
                     className="nav-link dropdown-toggle"
                     id="DonationsDropdown"
@@ -343,8 +343,8 @@ const List = ({ i18n, state }) => {
                     aria-expanded="false"
                   >
                     {i18n.t("Donations")}
-                  </a> */}
-                  {/* <div
+                  </a>
+                  <div
                     className={donationsToggleBotton ? "dropdown-menu toggleButtonShow CharityDropdown" : "dropdown-menu CharityDropdown"}
                     aria-labelledby="DonationsDropdown"
                   >
@@ -373,8 +373,8 @@ const List = ({ i18n, state }) => {
                         </div>
                       </div>
                     </div>
-                  </div> */}
-                {/* </li> */}
+                  </div>
+                </li>
                 <li className="nav-item" onClick={() => setToggleBotton(false)}>
                   <Link link="/company" className="nav-link">
                     {i18n.t("Company")}
